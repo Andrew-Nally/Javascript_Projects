2e6db04f75b7c42b37354f0264a1a3ba2cb7810a
Andrew Nally <Andrew_Nally@outlook.com>
2021-09-23 10:18:47 -0700

    project 1

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("Hello, World!");
+
+document.write('Hello, world!');
+
+var A  = "Hi Andrew, I like cheese!";
+document.write(A);
---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/main2.js
@@ -0,0 +1,19 @@
+var b = "string me along";//assigns the string to variable b
+window.alert(b);//creates a window alert box displaying variable b
+
+document.write('Jacob told Danny, \"You\'re so crazy Danny "');//dissplays the string with appropriate punctuation
+
+document.write(" I love you, " + "You love me " + "we\'re a happy family");//concatenates all three stirings
+
+var b = " concatenated " + " string "//concatenate the two strings and assigns it to variable b
+document.write(b);// displays variable b
+
+var friends = "WeHoes", Roommate = " Jacob", crazy = "Danny", ex = "Matt ", bff = "Nick", neighbor = "Landon";
+document.write(ex); //assigns each strings to a type of friend variable
+
+var green = Roommate, blue = ex, pink = bff, purple = neighbor, red = crazy;//assigns each friend variable to a color 
+friends = red.fontcolor('red') + " " + green.fontcolor("green") + " " + blue.fontcolor("blue") + " " + pink.fontcolor("pink") + " " + purple.fontcolor("purple");
+document.write(friends);// assigns a color to the color variable and concatenates the friend variables together 
+
+var expression = (10 * 1000); // assigns a variable to the math expression
+document.write(expression);// displays the math expression
