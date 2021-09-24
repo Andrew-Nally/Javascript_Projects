e7248cd172b1f347d60fd442b34fb1dcae1b662c
Andrew Nally <Andrew_Nally@outlook.com>
2021-09-24 09:37:54 -0700

    infintity examples

--- Basic_JavaScript_Projects/Project5_Comparison_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_Comparison_type_coercion/JS/main.js
@@ -6,4 +6,6 @@ function my_Function(){//declares function
 document.getElementById("Test").innerHTML = 0/0;//cant divide 0/0 so result = NaN
 document.getElementById("Test1").innerHTML = isNaN('This is not a number');//asks if string is a not a number returns true
 document.getElementById("Test2").innerHTML = isNaN('-007');//asks if string is non a number and returns false
+document.getElementById("infinity").innerHTML = 6E666;
+document.getElementById("-infinity").innerHTML = -6E666;
 }
