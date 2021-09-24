05490c75c8474e49aa8a1cad863a6d5b797fc64c
Andrew Nally <Andrew_Nally@outlook.com>
2021-09-24 10:20:43 -0700

    comparison

--- Basic_JavaScript_Projects/Project5_Comparison_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_Comparison_type_coercion/JS/main.js
@@ -9,3 +9,27 @@ document.getElementById("Test2").innerHTML = isNaN('-007');//asks if string is n
 document.getElementById("infinity").innerHTML = 6E666;
 document.getElementById("-infinity").innerHTML = -6E666;
 }
+document.write("<br>" + (50 > 100));
+document.write("<br>"+(100 > 50));
+document.write("<br>"+(100 < 50));
+document.write("<br>"+(50 < 100));
+document.write("<br>" +(50 >= 50));
+document.write("<br>" + (50 != 100));
+
+console.log(4+4);
+console.log(10 < 5);
+console.log(5 == "dog");
+console.log( "cat" == "cat");
+console.log(100 === 100);
+console.log("cat" === 4);
+console.log (10 === '10');
+console.log( 5 === -5);
+console.log (20 > 10 && 10 > 5);
+console.log (10 + 6 < 10 && 10 + 7 < 20);
+console.log(5 == 5 || 5 > 2);
+console.log (3 > 5 || 5 > 10);
+
+function not_Function(){
+    document.getElementById("Not").innerHTML = !(10 > 5);
+    document.getElementById("Not2").innerHTML = !(1 > 2);
+}
